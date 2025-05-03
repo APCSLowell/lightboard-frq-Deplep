f02a33c569c399314fbd25fc2e8919ddf12c0757
Deplep <131609777+Deplep@users.noreply.github.com>
2025-05-03 15:30:45 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,15 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
+    for(int r = 0; r < numRows; r++){
+      for(int c = 0; c < numCols; c++){
+        if(Math.random() > 0.4){
+          lights[r][c] = false;
+        }
+        lights[r][c] = true;
+      }
+    }
 
   }
 
@@ -20,9 +28,18 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
-   
- 
+    int count = 0;
+    for(int r = 0; r < lights.length; r++){
+      if(lights[r][col] == true){
+        count += 1;
+      }
+    }
+   if(lights[row][col] == true && count % 2 == 0){
+     return false;
+   }
+   if(lights[row][col] == false && count % 3 == 0){
+     return true;
+   }
   }
   public boolean[][] getLights()
   {
